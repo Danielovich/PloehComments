8204aada5a9b6131011793b38c9d0b6e36653cef
Daniel Frost <df@jyskebank.dk>
2023-03-29 10:57:43 +0200

    manual test

---
+++ src/Tests/ManualTests.cs
@@ -0,0 +1,34 @@
+﻿using PloehComments;
+
+namespace Tests;
+
+public class ManualTests
+{
+    //[Fact]
+    [Fact(Skip = "manual")]
+    public async Task Append_Links_And_Save_Html_Document_For_All_Posts()
+    {
+        // what dir holds the posts 
+        var postDirectoryPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Posts");
+
+        // all posts in the dir
+        var postNameRetriver = new PostsNameRetriever(postDirectoryPath);
+        var posts = await postNameRetriver.PostsInDirectory() ?? Enumerable.Empty<string>();
+
+        foreach (var postFileName in posts)
+        {
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Posts", postFileName);
+
+            // load it
+            var postLoader = new PostLoader(filePath);
+            var blogpost = await postLoader.PostToStringAsync();
+
+            // parse it to html
+            var htmlPost = new HtmlDocumentPost();
+            var htmlDocument = await htmlPost.PostToHtmlDocumentAsync(blogpost);
+
+            await htmlPost.AppendCommentLinkAsync(htmlDocument);
+            await htmlPost.SaveDocumentAsync(htmlDocument, postFileName);
+        }
+    }
+}
--- src/Tests/PostCommentsTests.cs
+++ src/Tests/PostCommentsTests.cs
@@ -36,31 +36,4 @@ public class PostCommentsTests
         Assert.True(blogpost.IndexOf("<a href=\"#95ce4e4b469847cea860f8a778ed1b6a\">#</a>") > 0);
         Assert.True(blogpost.IndexOf("<a href=\"#05d451209f714c029c143fd420c6ff99\">#</a>") > 0);
     }
-
-    [Fact]
-    public async Task Append_Links_And_Save_Html_Document_For_All_Posts()
-    {
-        // what dir holds the posts 
-        var postDirectoryPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Posts");
-
-        // all posts in the dir
-        var postNameRetriver = new PostsNameRetriever(postDirectoryPath);
-        var posts = await postNameRetriver.PostsInDirectory() ?? Enumerable.Empty<string>();
-            
-        foreach (var postFileName in posts)
-        {
-            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Posts", postFileName);
-
-            // load it
-            var postLoader = new PostLoader(filePath);
-            var blogpost = await postLoader.PostToStringAsync();
-
-            // parse it to html
-            var htmlPost = new HtmlDocumentPost();
-            var htmlDocument = await htmlPost.PostToHtmlDocumentAsync(blogpost);
-
-            await htmlPost.AppendCommentLinkAsync(htmlDocument);
-            await htmlPost.SaveDocumentAsync(htmlDocument, postFileName);
-        }
-    }
 }
